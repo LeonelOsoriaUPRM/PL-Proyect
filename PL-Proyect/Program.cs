dd5cb5a253d298005c0e6ff92bb38218d30269d9
rose.sanchez@upr.edu <rose.sanchez@upr.edu>
2021-12-07 03:06:38 -0800

    //Obsolete Branch!

--- PL-Proyect/Program.cs
+++ PL-Proyect/Program.cs
@@ -15,6 +15,7 @@ namespace PL_Proyect
         [STAThread]
         private static void Main(string[] args)
         {
+            //Obsolete Branch!
             /*
             Console.WriteLine("Hello World, we're making a compiler baby!");
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
